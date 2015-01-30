d59fdce1545036cdd40cbc1396d384c938f82b85
Carlos Velasquez <cv.velasquez@gmail.com>
2015-01-30 03:47:00 +0000

    08-arrays

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,11 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(name1,name2,name3,name4) { 
+    return [name1,name2,name3,name4];
+}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function (array) {
+    var lastElementIndex = array.length - 1;
+    var firstElement = array[0];
+    var lastElement = array[lastElementIndex];
+    return [firstElement, lastElement];
+}
+
