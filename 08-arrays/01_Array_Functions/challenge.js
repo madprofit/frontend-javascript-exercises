c2baf9648a12235f58b57c6ee5b606e111362420
Carlos Velasquez <cv.velasquez@gmail.com>
2015-01-30 05:38:21 +0000

    08-01-Array_Functions

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,16 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(x,y) {
+    var array = x,y;
+    array.reverse();
+    array.unshift(1);
+    return array;
+}
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(x,y,z) {
+    var array = x,y,z;
+    return array.join('+');
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(x,y,z) {
+    var array = x,y,z;
+    return array.length + 1;
+};
