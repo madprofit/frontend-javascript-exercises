89634c085c4039479f54bfe10ee1981cd00cb9f4
Carlos Velasquez <cv.velasquez@gmail.com>
2015-01-31 21:38:58 +0000

    09-objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,36 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+    return course = {
+        title: courseTitle
+        duration: courseDuration
+        students: courseStudents
+    }
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+     if (object[newProp]) {
+        return object;
+     }
+     else (object[newProp]) = newValue {
+        return object;
+     }
+};
 
-module.exports.formLetter = undefined;
 
-module.exports.canIGet = undefined;
+
+
+module.exports.formLetter = function(letter) {
+    return "Hello" + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+}
+
+module.exports.canIGet = function(item, money) {
+    pieChart = {
+        MacBook Air: 999,
+        MacBook Pro: 1299,
+        Mac Pro: 2499,
+        Apple Sticker: 1
+    };
+    return pieChart[item] <= money;
+};
+
+
+
