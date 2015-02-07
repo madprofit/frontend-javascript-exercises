7889180a71e18c414c8d9773bcd8ba97771a08c5
Carlos Velasquez <cv.velasquez@gmail.com>
2015-02-07 23:03:16 +0000

    00-looping0ver-arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,19 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  var sum = 0;
+  for (var counter = 0; counter < array.length; counter++) {
+    sum += array[counter];
+  }
+  return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+    return inputString.toLowerCase().split(",");
+}
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i ++) {
+        newArray[i] = i + " is " + array[i];
+}
+    return newArray;
+    };
