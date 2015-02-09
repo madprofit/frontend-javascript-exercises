7dc4276c8053057c60b5a002d760fe8b3e71d544
Carlos Velasquez <cv.velasquez@gmail.com>
2015-02-09 07:04:47 +0000

    10-loops

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,25 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+  var newObject = {};
+  for (var key in object) {
+    newObject[key] = object[key];   
+  }
+  return newObject;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+    for (var key in src) {
+        dest[key] = src[key];
+    }
+    return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, array) {
+  for (var i = 0; i < array.length; i++) {
+    var marker = array[i];
+
+    if(!object.hasOwnProperty(marker)) {
+      return false;
+    }
+  }
+  return true;
+};
